2b4be3d7f256486cb966aba98fbf06c35306c39e
Thatchaphon Sritrakul <Thatchaphon.Sri@truedigital.com>
2020-12-11 16:34:29 +0700

    Create Reporter project.

---
+++ Reporter/ReporterApp.swift
@@ -0,0 +1,20 @@
+//
+//  ReporterApp.swift
+//  Reporter
+//
+//  Created by Thatchaphon Sritrakul on 11/12/2563 BE.
+//
+
+import SwiftUI
+
+@main
+struct ReporterApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
