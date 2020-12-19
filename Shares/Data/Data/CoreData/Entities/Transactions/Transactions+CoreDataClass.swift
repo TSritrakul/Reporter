089a9ea6f08c4ecc796dce6d700a667eb154821f
Thatchaphon Sritrakul <Thatchaphon.Sri@truedigital.com>
2020-12-19 23:46:51 +0700

    Create Entity for Transaction.

---
+++ Shares/Data/Data/CoreData/Entities/Transactions/Transactions+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Transactions+CoreDataClass.swift
+//  Data
+//
+//  Created by Thatchaphon Sritrakul on 19/12/2563 BE.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Transactions)
+public class Transactions: NSManagedObject {
+
+}
---
+++ Shares/Data/Data/CoreData/Entities/Transactions/Transactions+CoreDataProperties.swift
@@ -0,0 +1,30 @@
+//
+//  Transactions+CoreDataProperties.swift
+//  Data
+//
+//  Created by Thatchaphon Sritrakul on 19/12/2563 BE.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Transactions {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Transactions> {
+        return NSFetchRequest<Transactions>(entityName: "Transactions")
+    }
+
+    @NSManaged public var symbol: String?
+    @NSManaged public var action: String?
+    @NSManaged public var date: Date?
+    @NSManaged public var price: String?
+    @NSManaged public var size: String?
+    @NSManaged public var commission: String?
+
+}
+
+extension Transactions : Identifiable {
+
+}
